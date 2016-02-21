94dca93cc18712362b35098a60416718c43af569
EtienneWan <etienne.wan@yahoo.com>
2016-02-21 22:03:35 +0000

    updated genManifest.php: blacklist of unused files

--- genManifest.php
+++ genManifest.php
@@ -1,13 +1,19 @@
 <?php
 
+$uselessFiles = array(
+   "assets/CHARS/IMAGES/CHME01BK.JPG",
+   "assets/TECH/IMAGES/TECHMENU.JPG"
+);
+
 function dirToManifest($dir) {
+   global $uselessFiles;
   // http://php.net/manual/fr/function.scandir.php#110570
   // Browse recursively directory and construct relative path to each file
    $result = "";
    $cdir = scandir($dir);
    foreach ($cdir as $key => $value)
    {
-      if (!in_array($value,array(".","..")))
+      if (!in_array($value, array(".","..")))
       {
          if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
          {
@@ -15,7 +21,9 @@ function dirToManifest($dir) {
          }
          else
          {
-            $result .= $dir."/".$value.PHP_EOL;
+            if (!in_array($dir."/".$value, $uselessFiles)){
+               $result .= $dir."/".$value.PHP_EOL;
+            }
          }
       }
    }
@@ -28,16 +36,21 @@ $data .= "#".date("Y-m-d H:i:s").PHP_EOL.PHP_EOL;
 $data .= "CACHE:".PHP_EOL.PHP_EOL;
 
 $data .= "index.html".PHP_EOL;
-$data .= "css/app.css".PHP_EOL;
-$data .= "app.js".PHP_EOL.PHP_EOL;
+//$data .= "css/app.css".PHP_EOL;
+//$data .= "app.js".PHP_EOL.PHP_EOL;
+$data .= "css/fonts/mech_monoline-webfont.eot".PHP_EOL;
+$data .= "css/fonts/mech_monoline-webfont.svg".PHP_EOL;
+$data .= "css/fonts/mech_monoline-webfont.ttf".PHP_EOL;
+$data .= "css/fonts/mech_monoline-webfont.woff".PHP_EOL;
+$data .= "css/fonts/mech_monoline-webfont.woff2".PHP_EOL.PHP_EOL;
+
 
 $data .= dirToManifest("assets");
 
 $data .= "NETWORK:".PHP_EOL;
-$data .= "*".PHP_EOL.PHP_EOL;
-$data .= "FALLBACK:".PHP_EOL;
-$data .= "*";
+$data .= "css/app.css".PHP_EOL;
+$data .= "app.js".PHP_EOL;
 
-$file = fopen("cache.manifest", "r+") or die("Unable to open file!");
+$file = fopen("cache.manifest", "w") or die("Unable to open file!");
 fwrite($file, $data);
 fclose($file);
